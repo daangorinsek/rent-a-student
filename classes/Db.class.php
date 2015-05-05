cfcd982f04d898482fefcb402b717e18dbdb9341
radinomike <rmike.be@gmail.com>
2015-05-05 13:21:05 +0200

    hompage and instagram feed

--- classes/Db.class.php
+++ classes/Db.class.php
@@ -4,11 +4,9 @@
 		private static $db;
 		public static function getInstance() {
 			if (self::$db === null) {
-				//echo "new connection";
 				self::$db = new PDO('mysql:host=localhost; dbname=rent_a_student', "root", "");
 				return self::$db;
 			} else {
-				//echo "same connection";
 				return self::$db;
 			}
 		}
--- index.php
+++ index.php
@@ -4,33 +4,96 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8"/>
-  <title>Rent-a-Student</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="language" content="english" />
   <meta name="keywords" content=""/>
   <meta name="author" content=""/>
-
-  <meta property="og:title" content="">
-  <meta property="og:description" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
-
+  
+  <title>Rent-a-Student</title>
   <link rel="shortcut icon" href=""/>
+
   <link rel="stylesheet" type="text/css" href="css/reset.css" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
   <link rel="stylesheet" type="text/css" href="css/style.css"/>
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+
+  <script type="text/javascript" src="js/instafeed.min.js"></script> 
+  <script type="text/javascript">
+    var feed = new Instafeed({
+      get: 'tagged',
+      tagName: 'weAreImd',
+      clientId: 'de691c9528ec484ea7360fa73c38b189',
+      template: '<figure><a href="{{link}}" target="_blank"><img src="{{image}}"/><figcaption>{{caption}}</figcaption></a></figure>',
+      //blocked the user imtoofabluv that uses the same hashtag but is not relevant
+      filter: function(image) {
+        var blockedUsernames = ['imtoofabluv'];
+          for (var i=0; i<blockedUsernames.length; i++) {
+            if (image.user.username === blockedUsernames[i]) {
+              return false;
+            }
+          }
+        return true;
+      }
+    });
+    feed.run();
+  </script>
+
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
 
 <body>
 
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Rent a Student</a>
+        <a href="signin.php" class="btn btn-success navbar-btn pull-right">Sign In</a>
+    </div>
+  </nav>
+
+  <div class="jumbotron">
+    <div class="container">
+      <h1>WeAreIMD</h1>
+      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      <form id="subscribe-form" action="" method="post">
+        <div class="input-group input-group-lg">
+          <input type="text" class="form-control" placeholder="your email address">
+          <span class="input-group-btn">
+            <button class="btn btn-primary" type="button">Subscribe</button>
+          </span>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Bezoekers</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-primary" href="signup_visitors.php" role="button">Sign Up</a></p>
+      </div>
+      <div class="col-md-6">
+        <h2>IMDers</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-primary" href="signup_students.php" role="button">Sign Up</a></p>
+      </div>
+    </div>
+    <hr>
+  </div> <!-- /container -->
+
+  <div class="container">
+    <div class="row">
+      <div id="instafeed"></div>
+    </div>
+  </div> <!-- /container -->
+
+
+<!-- SCRIPTS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/script.min.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 
 </body>
-
 </html>
